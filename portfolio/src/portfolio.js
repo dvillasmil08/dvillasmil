dd282853d8cf59f37dd6cfa13dd7e80678babf64
dvillasmil08 <dvillasmil08@gmail.com>
2022-05-09 20:32:09 -0400

    created Header.js and portfolio.js

---
+++ portfolio/src/portfolio.js
@@ -0,0 +1,22 @@
+const header = {
+    homepage: 'https://dvillasmil08.github.io',
+    title: 'JS.',
+}
+
+const about = {
+
+}
+
+const projects = {
+
+}
+
+const skills = {
+
+}
+
+const contact = {
+
+}
+
+export { header, about, projects, skills, contact }
