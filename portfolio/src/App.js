da4c29f89217afae07e96571abefad2d4406a69a
dvillasmil08 <dvillasmil08@gmail.com>
2022-05-09 20:49:12 -0400

    started Navbar a tags

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Header from './components/Header/Header'
+import About from './components/About/About'
+import Projects from './components/Projects/Projects'
+import Skills from './components/Skills/Skills'
+import Contact from './components/Contact/Contact'
+import ScrollToTop from './components/ScrollToTop/ScrollToTop'
+import Footer from './components/Footer/Footer'
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+        <h1>TEST</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header />
+        <About />
+        <Projects />
+        <Skills />
+        <Contact />
+        <ScrollToTop />
+        <Footer />
     </div>
   );
 }
