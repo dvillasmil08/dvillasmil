4ae913f65456102d15447b3ecba39de39ab4dfac
dvillasmil08 <dvillasmil08@gmail.com>
2022-05-10 19:59:23 -0400

    navbar basic info

---
+++ portfolio/src/components/Navbar/Navbar.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Navbar = () => {
+    return(
+        <nav className='center nav'>
+            <ul className='nav__list'
+            >
+                <li>
+                    <a>
+                        Projects
+                    </a>
+                </li>
+                <li>
+                    <a>
+                        Skills
+                    </a>
+                </li>
+                <li>
+                    <a>
+                        Contact
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
