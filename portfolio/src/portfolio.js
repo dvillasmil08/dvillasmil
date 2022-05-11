5d5b8704447f93d10b274b03da468c2da7f79a92
dvillasmil08 <dvillasmil08@gmail.com>
2022-05-10 20:13:12 -0400

    added data and placeholder

---
+++ portfolio/src/portfolio.js
@@ -0,0 +1,73 @@
+const header = {
+    homepage: 'https://dvillasmil08.github.io/',
+    title: 'JS.',
+}
+
+const about = {
+    name: 'Daniel Villasmil',
+    role: 'Software Developer',
+    description:
+        'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
+    resume: 'placeholder.com',
+    social: {
+        linkedin: 'https://www.linkedin.com/danielvillasmil/',
+        github: 'https://github.com/dvillasmil08/'
+    }
+}
+
+const projects = [
+    {
+        name: 'Project 1',
+        description:
+            'Here is project description',
+        stack: ['here is tech stack'],
+        sourceCode: 'github code link',
+        livePreview: 'github code link',
+
+},
+{
+    name: 'Project 2',
+    description:
+        'Here is project description',
+    stack: ['here is tech stack'],
+    sourceCode: 'github code link',
+    livePreview: 'github code link',
+
+},
+{
+    name: 'Project 3',
+    description:
+        'Here is project description',
+    stack: ['here is tech stack'],
+    sourceCode: 'github code link',
+    livePreview: 'github code link',
+
+},
+{
+    name: 'Project 4',
+    description:
+        'Here is project description',
+    stack: ['here is tech stack'],
+    sourceCode: 'github code link',
+    livePreview: 'github code link',
+
+},
+]
+
+const skills = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Git',
+    'CI/CD',
+    'Java',
+    'Docker',
+    'Kubernetes',
+]
+
+const contact = {
+    email: 'dvillasmil08@gmail.com'
+}
+
+export { header, about, projects, skills, contact }
