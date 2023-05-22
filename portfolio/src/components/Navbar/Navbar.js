3b590803eb5c15b4f7c211be92753931f0015a12
Daniel Villasmil <danielvillasmil@Daniels-MacBook-Pro.local>
2023-05-22 12:24:01 -0400

    corrected nav bar with Home, Projects, Hobbies and Contanct items

--- portfolio/src/components/Navbar/Navbar.js
+++ portfolio/src/components/Navbar/Navbar.js
@@ -6,17 +6,28 @@ const Navbar = () => {
     
 return (
     <nav className="navbar">
-        <button className="navbar__item navbar__theme-toggle">
-            <i className="fas fa-sun">light</i>
-            <i className="fas fa-moon">dark</i>
-        </button>
-        <button className="navbar__item navbar__hamburger">toggle</button>
-        {/* <div className="navbar__title navbar__item">Daniel Villasmil</div> */}
-        <div className="navbar__item navbar__list">
-            <div className="navbar__item navbar__link">Projects</div>
-            <div className="navbar__item navbar__link">Skills</div>
-            <div className="navbar__item navbar__link">Contact</div>
-        </div>
+        <ul className="navbar-nav">
+            <li className="nav-item">
+                <a href="#about" className="nav-link">
+                    Home
+                </a>
+            </li>
+            <li className="nav-item">
+                <a href="#skills" className="nav-link">
+                    Projects
+                </a>
+            </li>
+            <li className="nav-item">
+                <a href="#projects" className="nav-link">
+                    Hobbies
+                </a>
+            </li>
+            <li className="nav-item">
+                <a href="#contact" className="nav-link">
+                    Contact
+                </a>
+            </li>
+        </ul>
     </nav>
 )
 }
