2b9b012feeff5d842f3c42d182eef01a06149f6b
Daniel Villasmil <danielvillasmil@Daniels-MacBook-Pro.local>
2023-05-22 12:17:17 -0400

    moved projects to its own page and making landing page an about me

--- portfolio/src/components/Body/Body.js
+++ portfolio/src/components/Body/Body.js
@@ -1,20 +1,14 @@
 import React from 'react'
-import {project} from '../../portfolio'
+import Footer from '../Footer/Footer'
 import './Body.css'
+import Header from '../Header/Header'
 
 const body = () => {
     return (
-    <div className='card'>
-    {project.map((project) => (
-        <div className='card-body'>
-            <h3 className='card-title'>{project.name}</h3>
-            <p className='card-text'>{project.description}</p>
-            <a href={project.homepage} className='card-link'>Link to project</a>
-            <a href={project.repository} className='card-link'>Link to repository</a>
+        <div>
+            About me
         </div>
-    ))}
-</div>
-    )
+)
 }
 
     export default body
