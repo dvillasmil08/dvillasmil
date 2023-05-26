ded6cfe0522e4e82adc5440b8fc1ce36af2c629e
dvillasmil08 <dvillasmil08@gmail.com>
2023-05-26 09:46:21 -0400

    added logic if the logo has a link or not

--- portfolio/src/components/Common/Logo.js
+++ portfolio/src/components/Common/Logo.js
@@ -1,8 +1,16 @@
 import { INFO } from "../../data/user";
 import "./styles/Logo.css";
+import { Link } from "react-router-dom";
 
 const Logo = (props) => {
+  let { width, link } = props;
+  if (link === undefined) {
+    link = true; // Set link prop to true if not provided
+  }
+  const imageElement = (
+    <img src={INFO.main.logo} alt="logo" className="logo" width={width} />
+  );
+  return <>{link ? <Link to="/">{imageElement}</Link> : imageElement}</>;
+};
 
- }
-
- export default Logo;
+export default Logo;
