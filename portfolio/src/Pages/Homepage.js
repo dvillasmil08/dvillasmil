c6b0ec5ff15d4a5f5a0bf040327db0804aec6290
dvillasmil08 <dvillasmil08@gmail.com>
2023-05-31 09:39:51 -0400

    forgot what this commit was about

--- portfolio/src/Pages/Homepage.js
+++ portfolio/src/Pages/Homepage.js
@@ -16,6 +16,30 @@ return (
             <div className="subtitle homepage-subtitle">
                 {INFO.homepage.description}
             </div>
+
+        <div className="homepage-image-wrapper">
+            <img src="../Images/profile.jpeg" alt="about" className="homepage-image" />
+        </div>
+
+        <div className="homepage-socials">
+            <a href={INFO.socals.github}>
+
+            </a>
+            <a href={INFO.socals.stackOverflow}>
+                
+            </a>
+            <a href={INFO.socals.instagram}>
+                
+            </a>
+            <a href={INFO.main.email}>
+                
+            </a>
+        </div>
+
+
+
+
+
         </div>
     </div>
 </div>
