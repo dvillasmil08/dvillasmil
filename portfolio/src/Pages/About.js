e6c8c19a52f63c0ad24ab994fd1a18e7964e9903
dvillasmil08 <dvillasmil08@gmail.com>
2023-06-12 13:02:14 -0400

    added image to About me page

--- portfolio/src/Pages/About.js
+++ portfolio/src/Pages/About.js
@@ -50,7 +50,7 @@ const About = () => {
               <div className="about-left-side">
                 <div className="about-image-container">
                   <div className="about-image-wrapper">
-                    <img src="about.jpg" alt="about" className="about-image" />
+                    <img src="esports.JPG" alt="about" className="about-image" />
                   </div>
                 </div>
 
