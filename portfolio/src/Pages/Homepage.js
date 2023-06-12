baeccfa4424752873065dc8d7f2ae31ba1959ba7
dvillasmil08 <dvillasmil08@gmail.com>
2023-06-12 11:01:40 -0400

    sets up state variables to keep track of the logo size and its position based on the scroll position

--- portfolio/src/Pages/Homepage.js
+++ portfolio/src/Pages/Homepage.js
@@ -1,77 +1,133 @@
 import NavBar from "../components/Common/Navbar";
 import { Helmet } from "react-helmet";
 import { INFO } from "../data/user";
+import SEO from "../data/seo";
 import Footer from "../components/Common/Footer";
 import "./styles/homepage.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope } from '@fortawesome/fontawesome-free';
-import {faGithub, faStackOverflow, faInstagram} from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/fontawesome-free";
+import {
+  faGithub,
+  faStackOverflow,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+import { useEffect, useState } from "react";
 
 const Homepage = () => {
-return (
+  const [stayLogo, setStayLogo] = useState(false);
+  const [logoSize, setLogoSize] = useState(80);
+  const [oldLogoSize, setOldLogoSize] = useState(80);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      let scroll = Math.round(window.scrollYOffset, 2);
+
+      let newLogoSize = 80 - (scroll * 4) / 10;
+
+      if (newLogoSize < oldLogoSize) {
+        if (newLogoSize > 40) {
+          setLogoSize(newLogoSize);
+          setOldLogoSize(newLogoSize);
+          setStayLogo(false);
+        } else {
+          setStayLogo(true);
+        }
+      } else {
+        setLogoSize(newLogoSize);
+        setStayLogo(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [logoSize, oldLogoSize]);
+
+  const currentSEO = SEO.find((item) => item.page === "home");
+
+  const logoStyle = {
+    display: "flex",
+    position: stayLogo ? "fixed" : "relative",
+    top: stayLogo ? "3vh" : "auto",
+    zIndex: 999,
+    border: stayLogo ? "1px solid white" : "none",
+    borderRadius: stayLogo ? "50%" : "none",
+    boxShadow: stayLogo ? "0px 4px 10px rgba(0, 0, 0, 0.25)" : "none",
+  };
+
+  return (
     <>
-<Helmet>
-            <title className="title homepage-title">
-                {INFO.main.title}
-            </title>
+      <Helmet>
+        <title className="title homepage-title">{INFO.main.title}</title>
+        <meta name="description" content={currentSEO.description} />
+        <meta name="keywords" content={currentSEO.keywords.join(",")} />
+      </Helmet>
 
-</Helmet>
-
-<div className="page-content">
-    <NavBar active="home" />
-    <div className="content-wrapper">
-        <div className="homepage-container">
-           <div className="homepage-first-area">
-            <div className="homepage-first-area-left-side">
+      <div className="page-content">
+        <NavBar active="home" />
+        <div className="content-wrapper">
+          <div className="homepage-container">
+            <div className="homepage-first-area">
+              <div className="homepage-first-area-left-side">
                 <div className="title homepage-title">
-                    {INFO.homepage.title}
+                  {INFO.homepage.title}
                 </div>
 
                 <div className="subtitle homepage-subtitle">
-                    {INFO.homepage.description}
+                  {INFO.homepage.description}
                 </div>
-            </div>
+              </div>
 
-            <div className="homepage-first-area-right-side">
+              <div className="homepage-first-area-right-side">
                 <div className="homepage-image-wrapper">
-                    <img src="profile.jpeg" alt="about" className="homepage-image" />
+                  <img
+                    src="profile.jpeg"
+                    alt="about"
+                    className="homepage-image"
+                  />
                 </div>
-
+              </div>
             </div>
-        
 
-        </div>
-
-        <div className="homepage-socials">
-            <a href={INFO.socials.github}>
+            <div className="homepage-socials">
+              <a href={INFO.socials.github}>
                 <FontAwesomeIcon icon={faGithub} className="homepage-social" />
-            </a>
-            <a href={INFO.socials.stackOverflow}>
-                <FontAwesomeIcon icon={faStackOverflow} className="homepage-social" />
-            </a>
-            <a href={INFO.socials.instagram}>
-                <FontAwesomeIcon icon={faInstagram} className="homepage-social" />
-            </a>
-            <a href={`mailto:${INFO.main.email}`}
-								target="_blank"
-								rel="noreferrer">
-                <FontAwesomeIcon icon={faEnvelope} className="homepage-social" />
-            </a>
+              </a>
+              <a href={INFO.socials.stackOverflow}>
+                <FontAwesomeIcon
+                  icon={faStackOverflow}
+                  className="homepage-social"
+                />
+              </a>
+              <a href={INFO.socials.instagram}>
+                <FontAwesomeIcon
+                  icon={faInstagram}
+                  className="homepage-social"
+                />
+              </a>
+              <a
+                href={`mailto:${INFO.main.email}`}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FontAwesomeIcon
+                  icon={faEnvelope}
+                  className="homepage-social"
+                />
+              </a>
+            </div>
+
+            <div className="page-footer">
+              <Footer />
+            </div>
+          </div>
         </div>
-
-        <div className="page-footer">
-            <Footer />
-        </div>
-
-
-
-
-
-        </div>
-    </div>
-</div>
+      </div>
     </>
-);
-}
+  );
+};
 
 export default Homepage;
---
+++ portfolio/src/data/seo.js
@@ -0,0 +1,23 @@
+const SEO = [
+    {
+        page: "home",
+        description: "Ohio-born, Venezuela raised where I studied Civil Engineering, I discovered my passion for programming through a C++ class, realizing that software development is my true calling.",
+        keywords: ["Daniel, Daniel V, Daniel Villasmil"],
+    },
+    {
+        page: "about",
+        description: "Ohio-born, Venezuela raised where I studied Civil Engineering, I discovered my passion for programming through a C++ class, realizing that software development is my true calling.",
+        keywords: ["Daniel, Daniel V, Daniel Villasmil"],
+    },
+    {
+        page: "projects",
+        description: "I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to.",
+        keywords: ["Daniel, Daniel V, Daniel Villasmil"],
+    },
+    {   page: "contact",
+        description: "If you're interested in collaborating on a project, feel free to reach out to me. I'm always open to new ideas and opportunities.",
+        keywords: ["Daniel, Daniel V, Daniel Villasmil"],
+    },
+];
+
+export default SEO;
