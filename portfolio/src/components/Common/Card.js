53e85eb979db03753fc044fc5e6d921c287ed767
dvillasmil08 <dvillasmil08@gmail.com>
2023-06-12 12:46:35 -0400

    created About me page. Need to fix styling on portfolio

--- portfolio/src/components/Common/Card.js
+++ portfolio/src/components/Common/Card.js
@@ -1,22 +1,24 @@
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "./styles/Card.css";
+import "./style/Card.css"
 
 const Card = (props) => {
-    const { icon, title, body } = props;
-  return (
-    <div className="card">
-      <div className="card-constainer">
-        <div className="card-header">
-          <div className="card-icon">
-            <FontAwesomeIcon icon={props.icon} />
-          </div>
-          <div className="card-title">{title}</div>
-        </div>
-        <div className="card-body">
-          <div className="card-text">{body}</div>
-        </div>
-      </div>
-    </div>
-  );
+	const { icon, title, body } = props;
+	return (
+		<div className="card">
+			<div className="card-container">
+				<div className="card-header">
+					<div className="card-icon">
+						<FontAwesomeIcon icon={icon} />
+					</div>
+					<div className="card-title">{title}</div>
+				</div>
+				<div className="card-body">
+					<div className="card-text">{body}</div>
+				</div>
+			</div>
+		</div>
+	);
 };
+
 export default Card;
--- portfolio/src/components/Common/Footer.js
+++ portfolio/src/components/Common/Footer.js
@@ -1,41 +1,35 @@
 import React from "react";
-// import "./Footer.css";
+import "./styles/Footer.css";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
-  const currentYear = new Date().getFullYear();
 
   return (
-    <div className="footer">
-      <div className="footer-links">
-        <ul className="footer-nav-links-list">
-          <li className="footer-nav-link-item">
-            <Link to="/">Home</Link>
-          </li>
-
-          <li className="footer-nav-link-item">
-            <Link to="about">About</Link>
-          </li>
-
-          <li className="footer-nav-link-item">
-            <Link to="projects">Projects</Link>
-          </li>
-
-          <li className="footer-nav-link-item">
-            <Link to="hobbies">Hobbies</Link>
-          </li>
-
-          <li className="footer-nav-link-item">
-            <Link to="contact">Contact</Link>
-          </li>
-        </ul>
+    <>
+      <div className="footer">
+        <div className="footer-links">
+          <ul className="footer-nav-link-list">
+            <li className="footer-nav-link-item">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="footer-nav-link-item">
+              <Link to="/about">About</Link>
+            </li>
+            <li className="footer-nav-link-item">
+              <Link to="/projects">Projects</Link>
+            </li>
+            <li className="footer-nav-link-item">
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
       </div>
       <div className="footer-credits">
         <div className="footer-credits-text">
-          <p>{currentYear} DanielVillasmil.dev. All Rights Reserved.</p>
+        © 2023 DanielVillasmil.dev. All Rights Reserved.
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
