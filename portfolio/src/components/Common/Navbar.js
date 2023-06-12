ad01f9e325dd85a69af26934151869d090a30c47
dvillasmil08 <dvillasmil08@gmail.com>
2023-06-12 12:03:42 -0400

    added styling and centered navBar

--- portfolio/src/components/Common/Navbar.js
+++ portfolio/src/components/Common/Navbar.js
@@ -1,52 +1,68 @@
 import React from "react";
-import "./styles/Navbar.css";
 import { Link } from "react-router-dom";
+
+import "./styles/Navbar.css";
 
 const NavBar = (props) => {
 	const { active } = props;
 
-  return (
-    <div className="nav-container">
-      <nav className="navbar">
-        <div className="nav-background">
-          <ul className="nav-list">
-            <li className={active === "home" ? "nav-item active" : "nav-item"}>
-              <Link to="/">Home</Link>
-            </li>
-
-            <li className={active === "about" ? "nav-item active" : "nav-item"}>
-              <Link to="/about" className="nav-link">
-                About
-              </Link>
-            </li>
-
-            <li
-              className={active === "projects" ? "nav-item active" : "nav-item"}
-            >
-              <Link to="/projects" className="nav-link">
-                Projects
-              </Link>
-            </li>
-
-            <li
-              className={active === "hobbies" ? "nav-item active" : "nav-item"}
-            >
-              <Link to="/hobbies" className="nav-link">
-                Hobbies
-              </Link>
-            </li>
-            <li
-              className={active === "contact" ? "nav-item active" : "nav-item"}
-            >
-              <Link to="/contact" className="nav-link">
-                Contact
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </div>
-  );
+	return (
+		<React.Fragment>
+			<div className="nav-container">
+				<nav className="navbar">
+					<div className="nav-background">
+						<ul className="nav-list">
+							<li
+								className={
+									active === "home"
+										? "nav-item active"
+										: "nav-item"
+								}
+							>
+								<Link to="/">Home</Link>
+							</li>
+							<li
+								className={
+									active === "about"
+										? "nav-item active"
+										: "nav-item"
+								}
+							>
+								<Link to="/about">About</Link>
+							</li>
+							<li
+								className={
+									active === "projects"
+										? "nav-item active"
+										: "nav-item"
+								}
+							>
+								<Link to="/projects">Projects</Link>
+							</li>
+							<li
+								className={
+									active === "articles"
+										? "nav-item active"
+										: "nav-item"
+								}
+							>
+								<Link to="/articles">Articles</Link>
+							</li>
+							<li
+								className={
+									active === "contact"
+										? "nav-item active"
+										: "nav-item"
+								}
+							>
+								<Link to="/contact">Contact</Link>
+							</li>
+						</ul>
+					</div>
+				</nav>
+			</div>
+		</React.Fragment>
+	);
 };
 
 export default NavBar;
